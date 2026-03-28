693072f677367fbbe4be06e6f4124362fe3ce0f5
Shallum Mbugua <mbuguashallom@gmail.com>
2026-03-28 21:45:32 +0300

    Completed login tracker lab

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+function createLoginTracker(userInfo) {
+  // Initialize Attempt Count
+  let attemptCount = 0;
 
+  // Create and return arrow function
+  return (passwordAttempt) => {
+    // Increment attemptCount by 1
+    attemptCount++;
 
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+    // Check if password matches and attempts are 3 or less
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Login successful";
+    } 
+    // If they don't match, but they haven't exceeded 3 attempts yet
+    else if (attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
+    } 
+    // If they have exceeded 3 attempts
+    else {
+      return "Account locked due to too many failed login attempts";
+    }
+  };
+}
